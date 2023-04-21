3c10acf2059e97b9202e95b5ad37d73ba4fc7148
Andrew <andrewcrink@gmail.com>
2023-04-20 21:38:16 -0400

    react skeleton functional

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+        <div>
+            <h1>Welcome to The Final Stretch</h1>
+            <img id="homeLogo" src= 'https://www.usnews.com/cmsmedia/13/7c/a9774d7f410785fa055cf06a6ffd/140707-seniorsexercise-stock.jpg'  alt="logo"/>
+        </div>
+    )
+}
+
+export default Home
---
+++ frontend/src/Components/NewWorkoutForm.js
@@ -0,0 +1,9 @@
+function NewWorkoutForm () {
+    return (
+        <div>
+            <h1>New Workout Form</h1>
+        </div>
+    )
+}
+
+export default NewWorkoutForm
