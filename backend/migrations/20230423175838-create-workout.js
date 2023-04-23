3455245bf16e2109ed11d8ca9415397b986628df
Ckey230 <Ckey230@gmail.com>
2023-04-23 14:03:22 -0400

    model/migration for workout

---
+++ backend/migrations/20230423175838-create-workout.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('workouts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      workout_id: {
+        type: Sequelize.INTEGER
+      },
+      author_id: {
+        type: Sequelize.STRING
+      },
+      workout_name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.SMALLINT
+      },
+      calories_burned: {
+        type: Sequelize.SMALLINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('workouts');
+  }
+};
