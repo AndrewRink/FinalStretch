f09338a288943d12434c76f3760abc4ad0076181
Graham <graham.lindley23@gmail.com>
2023-04-28 14:44:29 -0400

    added google login button to log in page

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,64 @@
 //Dependencies
 require('dotenv').config();
-require('./auth')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const { Sequelize } = require('sequelize')
-const cors = require('cors')
-const passport = require('passport')
+require('./auth');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
+const cors = require('cors');
+const passport = require('passport');
+const app = express();
 
 //configuration 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+//checks if user is logged in
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
 
 
 //root
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('<a href="/auth/google">Authenticate with Google</a>')
-})
+});
 
-app.get('/auth/google', 
-passport.authenticate('google', {scope: ['email', 'profile']})
-)
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['email', 'profile'] })
+);
 
-app.get('/google/callback', 
+app.get('/google/callback',
     passport.authenticate('google', {
-    successRedirect:'/protected',
-    failureRedirect:'/auth/failure',
+        successRedirect: '/protected',
+        failureRedirect: '/auth/failure',
     })
-)
+);
 
 app.get('/auth/failure', (req, res) => {
     res.send('something went wrong')
-})
+});
 
-app.get('/protected', (req, res) => {
-    res.send('Hello!');
-})
+app.get('/protected', isLoggedIn, (req, res) => {
+    res.send(`Hello ${req.user.displayName}`);
+});
 
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.send('Goodbye!');
+});
 //controllers
-const workoutController = require('./controllers/workout_controller')
-app.use('/workoutlist', workoutController)
+const workoutController = require('./controllers/workout_controller');
+app.use('/workoutlist', workoutController);
 
 //PORT
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port: ${process.env.PORT}`)
-})
+    console.log(`Listening on port: ${process.env.PORT}`);
+});
